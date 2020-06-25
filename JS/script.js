b1e5117431bf6ae2da1c3b4da16581bd9f1a21c5
Malathi <malathi25121998@gmail.com>
2020-06-25 12:39:10 +0530

    correction made

--- JS/script.js
+++ JS/script.js
@@ -7,6 +7,8 @@ var modal4 = document.getElementById("modal_certification");
 
 var modal_div_tech = document.getElementById("data_tech");
 var modal_div_ide = document.getElementById("data_ide");
+var modal_rate_program = document.getElementById("modal_rating_program");
+var modal_rate_front = document.getElementById("modal_rating_front");
 
 // Get the button that opens the modal
 var btn = document.getElementById("career_obj");
@@ -17,6 +19,8 @@ var cer_btn = document.getElementById("certification");
 
 var tech_btn = document.getElementById("technical");
 var ide_btn = document.getElementById("ide");
+var pro_btn = document.getElementById("rate_program");
+var front_btn = document.getElementById("rate_front");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -27,6 +31,8 @@ var span4 = document.getElementsByClassName("close")[4];
 
 var spanclose_1 = document.getElementsByClassName("close_btn")[0];
 var spanclose_2 = document.getElementsByClassName("close_btn")[1];
+var spanrateclose_1 = document.getElementsByClassName("rate_close_btn")[0];
+var spanrateclose_2 = document.getElementsByClassName("rate_close_btn")[1];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -51,6 +57,12 @@ tech_btn.onclick = function() {
 ide_btn.onclick = function() {
     modal_div_ide.style.display = "block";
 }
+pro_btn.onclick = function() {
+    modal_rate_program.style.display = "block";
+}
+front_btn.onclick = function() {
+    modal_rate_front.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -74,9 +86,16 @@ spanclose_1.onclick = function() {
     modal_div_tech.style.display = "none";
 }
 spanclose_2.onclick = function() {
-        modal_div_ide.style.display = "none";
-    }
-    // When the user clicks anywhere outside of the modal, close it
+    modal_div_ide.style.display = "none";
+}
+spanrateclose_1.onclick = function() {
+    modal_rate_program.style.display = "none";
+}
+spanrateclose_2.onclick = function() {
+    modal_rate_front.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
@@ -98,5 +117,11 @@ window.onclick = function(event) {
     }
     if (event.target == modal_div_ide) {
         modal_div_ide.style.display = "none";
+    }
+    if (event.target == modal_rate_program) {
+        modal_rate_program.style.display = "none";
+    }
+    if (event.target == modal_rate_front) {
+        modal_rate_front.style.display = "none";
     }
 }
